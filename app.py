b82ab9f5ae90043feb73d7c9b388603bbf57cb6f
Sekidde Derrick <kidricederek@gmail.com>
2018-10-25 09:54:29 +0300

    Changed product endpoint to int.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ api = Api(api_bp)
 
 api.add_resource(Index, '/')
 api.add_resource(ProductList, '/Products')
-api.add_resource(ProductOne, '/Products/<string:name>')
+api.add_resource(ProductOne, '/Products/<int:product_id>')
 api.add_resource(SaleList, '/Sales')
 api.add_resource(SaleOne, '/Sales/<int:sale_id>')
 api.add_resource(UserList, '/Users')
--- storeapp/products.py
+++ storeapp/products.py
@@ -8,7 +8,7 @@ from flask import request, jsonify
 
 products = [
     {
-        "id": 1,
+        "product_id": 1,
         "name" : "tops",
 	    "qty" : 12,
 	    "min_stock" : 23,
@@ -17,7 +17,7 @@ products = [
 	    "category" : "clothes"
     },
     {
-        "id": 2,
+        "product_id": 2,
         "name" : "tops",
 	    "qty" : 12,
 	    "min_stock" : 23,
@@ -27,14 +27,14 @@ products = [
     }
 ]
 
-def get_product_by_id(id):
+def get_product_by_id(product_id):
     for product in products:
-        if product.get("id") == id:
+        if product.get("product_id") == int(product_id):
             return product 
             
 
 product_request_parser = RequestParser(bundle_errors=True)
-product_request_parser.add_argument("id", type=int, required=True, help="Please enter a valid integer for id.")
+product_request_parser.add_argument("product_id", type=int, required=True, help="Please enter a valid integer for id.")
 product_request_parser.add_argument("name", type=str, required=True, help="name has to be a valid string")
 product_request_parser.add_argument("qty", type=int, required=True, help="Please enter a valid integer for qty")
 product_request_parser.add_argument("min_stock", type=int, required=True, help="Please enter a valid integer for min_stock")
@@ -43,30 +43,18 @@ product_request_parser.add_argument("units", type=int, required=True, help="Plea
 product_request_parser.add_argument("category", type=str, required=True, help="Category has to be a valid string")
 
 class Product:
-    def __init__(self, id, name, qty, min_stock, price, units, category):
-        self.id = id
+    def __init__(self, product_id, name, qty, min_stock, price, units, category):
+        self.product_id = product_id
         self.name = name
         self.qty = qty
         self.min_stock = min_stock
         self.price = price
         self.units = units
-        self.category = category
-
-    def add_product(self):
-        product = dict(
-            id = len(products)+1,
-            name = self.name,
-            qty = self.qty,
-            min_stock = self.min_stock,
-            price = self.price,
-            units = self.units,
-            category = self.category
-        )
-        products.append(product) 
+        self.category = category 
 
 class ProductOne(Resource):
-    def get(self, name):
-        product = get_product_by_id(id)
+    def get(self, product_id):
+        product = get_product_by_id(product_id)
         if not product:
             return {"error": "product not found"} 
         return product
