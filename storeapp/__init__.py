c33754a5739f8f065731ed283874ae3d782e0699
Sekidde Derrick <kidricederek@gmail.com>
2018-10-25 12:18:43 +0300

    Add routes file.

--- storeapp/__init__.py
+++ storeapp/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from app import api_bp
+    from routes import api_bp
     app.register_blueprint(api_bp, url_prefix='/api/v1')
 
     return app
