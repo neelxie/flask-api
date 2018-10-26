e48748b09b546db6460a36e8452bbebe99843a12
Sekidde Derrick <kidricederek@gmail.com>
2018-10-26 10:40:54 +0300

    Remove redundant code.

--- storeapp/models/products_model.py
+++ storeapp/models/products_model.py
@@ -1,5 +1,3 @@
-from flask_restful.reqparse import RequestParser
-
 
 class Product:
     def __init__(self, product_id, name, qty, min_stock, price, units, category):
@@ -10,13 +8,3 @@ class Product:
         self.price = price
         self.units = units
         self.category = category 
-
-
-product_request_parser = RequestParser(bundle_errors=True)
-product_request_parser.add_argument("product_id", type=int, required=True, help="Please enter a valid integer for id.")
-product_request_parser.add_argument("name", type=str, required=True, help="name has to be a valid string")
-product_request_parser.add_argument("qty", type=int, required=True, help="Please enter a valid integer for qty")
-product_request_parser.add_argument("min_stock", type=int, required=True, help="Please enter a valid integer for min_stock")
-product_request_parser.add_argument("price", type=int, required=True, help="Please enter a valid integer for price")
-product_request_parser.add_argument("units", type=int, required=True, help="Please enter a valid integer for units")
-product_request_parser.add_argument("category", type=str, required=True, help="Category has to be a valid string")
--- storeapp/models/sales_model.py
+++ storeapp/models/sales_model.py
@@ -1,4 +1,3 @@
-from flask_restful.reqparse import RequestParser
 
 
 class sale:
@@ -10,11 +9,3 @@ class sale:
         self.product_id = product_id
         self.category = category
 
-
-sale_request_parser = RequestParser(bundle_errors=True)
-sale_request_parser.add_argument("sale_id", type=int, required=True, help="Please enter a valid integer for sale_id.")
-sale_request_parser.add_argument("product_name", type=str, required=True, help="Product_name has to be a valid string")
-sale_request_parser.add_argument("qty_sold", type=int, required=True, help="Please enter a valid integer for qty_sold")
-sale_request_parser.add_argument("amount", type=int, required=True, help="Please enter a valid integer for amount")
-sale_request_parser.add_argument("product_id", type=int, required=True, help="Please enter a valid integer for product_id")
-sale_request_parser.add_argument("category", type=str, required=True, help="Category has to be a val  id string")
